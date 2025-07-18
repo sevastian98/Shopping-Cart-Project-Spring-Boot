815ca7f17f07e54caf966a08db21a15bdfe0dae5
Sevastian <sevastian.vascan7@gmail.com>
2025-07-18 09:54:39 +0300

    Initial commit

---
+++ src/test/java/pixel/academy/ShoppingCart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.ShoppingCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
